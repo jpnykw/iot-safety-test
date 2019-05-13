3206353869c1a49deb727533e0979d65664fd5b1
YoshikazuFusayasu <50490024+YoshikazuFusayasu@users.noreply.github.com>
2019-05-13 19:05:13 +0900

    Add files via upload
    (行数はかなり少ないけど)jsonとxml形式でのnmap出力
    json内、portの中に必要な情報はだいたい入ってると思います

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
-import nmap
 
+#### 動作に際して、2つのパッケージが必要 ####
+#pip3 install python-nmap
+#pip3 install xmltodict
+
+import nmap
+import json
+import xmltodict
+
+#localhostの0-65535(全ポート)に対してスキャン
 nm = nmap.PortScanner()
 nm.scan(hosts="localhost", arguments="-sS -p "+"0-65535")
-result =  nm.get_nmap_last_output()
+xml_result =  nm.get_nmap_last_output()	#xml形式での取得
+
+#### xml形式での出力(正直必要は無いです)
 with open("/home/rootuser/scan.xml","w") as f:
-	f.write(result)
+	f.write(xml_result)
+
+####json形式での出力(xml→json)
+with open("/home/rootuser/scan.json","w") as f:
+	f.write(json.dumps(xmltodict.parse(xml_result), indent=4, sort_keys=True))
+
+
 
