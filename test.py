ec7e596fbe5fbe074fde92f1a72dab33a4880e10
YoshikazuFusayasu <50490024+YoshikazuFusayasu@users.noreply.github.com>
2019-05-14 21:57:06 +0900

    Add files via upload
    どこかから読み出してlocalhost以外への実行を想定して : 
    python3 test.py localhost
    の様に引数での指定・実行形体に変更。
    localhost以外での実行は絶望的に遅いため(仮想環境→ホストマシンでも1時間以上必要(終わる気配なし))、python-nmap以外への仕様変更が必要かもしれません(ということで、暫定仕様)

--- test.py
+++ test.py
@@ -6,19 +6,19 @@
 import nmap
 import json
 import xmltodict
+import sys
 
 #localhostの0-65535(全ポート)に対してスキャン
+#localhost以外にも繋げる方が〜→「引数に入れてください」仕様に変更
 nm = nmap.PortScanner()
-nm.scan(hosts="localhost", arguments="-sS -p "+"0-65535")
+nm.scan(hosts=sys.argv[1], arguments="-sS -p "+"0-65535")
 xml_result =  nm.get_nmap_last_output()	#xml形式での取得
 
-#### xml形式での出力(正直必要は無いです)
-with open("/home/rootuser/scan.xml","w") as f:
-	f.write(xml_result)
+#### xml形式での出力→いらないのでコメントアウト(必要なら外してください)
+#with open("/home/rootuser/scan.xml","w") as f:
+#	f.write(xml_result)
 
 ####json形式での出力(xml→json)
 with open("/home/rootuser/scan.json","w") as f:
 	f.write(json.dumps(xmltodict.parse(xml_result), indent=4, sort_keys=True))
-
-
 
