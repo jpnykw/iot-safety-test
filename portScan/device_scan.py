e14b583724a5af5260f7cbd23296f5971dc6e3db
Takamichi Omori <merrytakamerry@gmail.com>
2019-05-15 12:53:03 +0900

    fix output

--- portScan/device_scan.py
+++ portScan/device_scan.py
@@ -2,9 +2,10 @@
 
 import nmap
 import json
+import sys
 
 nm = nmap.PortScanner()
-nm.scan(hosts="192.168.11.1/24", arguments="-n -sP -PE -PA21,23, 80, 3389")
+nm.scan(hosts=sys.argv[1]+"/24", arguments="-n -sP -PE -PA21,23, 80, 3389")
 #hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 
 hosts = {'hosts': nm.all_hosts()}
--- portScan/port_scan.py
+++ portScan/port_scan.py
@@ -6,12 +6,13 @@
 import nmap
 import json
 import xmltodict
+import sys
 
 #localhostの0-65535(全ポート)に対してスキャン
 nm = nmap.PortScanner()
-target_host = input()
-print("Scan Started to {}".format(target_host))
-nm.scan(hosts=target_host, arguments="-sS -p "+"0-65535")
+target_host = sys.argv[1]
+#print("Scan Started to {}".format(target_host))
+nm.scan(hosts=target_host,arguments="-T4 -f")
 xml_result =  nm.get_nmap_last_output()	#xml形式での取得
 
 #### xml形式での出力(正直必要は無いです)
@@ -19,8 +20,10 @@ xml_result =  nm.get_nmap_last_output()	#xml形式での取得
 #	f.write(xml_result)
 
 ####json形式での出力(xml→json)
-with open("scan.json","w") as f:
-	f.write(json.dumps(xmltodict.parse(xml_result), indent=4, sort_keys=True))
+output = json.dumps(xmltodict.parse(xml_result), indent=4, sort_keys=True)
+#with open("scan.json","w") as f:
+#	f.write(output)
+print(output)
 
 
 
