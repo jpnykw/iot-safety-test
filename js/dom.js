cb9986442977303a5757a81c63bcab6328b21010
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2019-05-18 00:32:06 +0900

    一旦

--- js/dom.js
+++ js/dom.js
@@ -12,17 +12,30 @@ const execSync = require('electron').remote.require('child_process').execSync;
 
     onload = () => {
         const pages = {
-            home: document.getElementById('home')
+            home: document.getElementById('home'),
+            detecting: document.getElementById('detecting')
         };
 
         buttons = [...document.getElementsByClassName('button')];
-        buttons[0].addEventListener('click', () => pages.home.style.animation = 'toHide 500ms ease-in-out forwards');
+        buttons[0].addEventListener('click', () => {
+            pages.home.style.animation = 'toHide 500ms ease-in-out forwards';
+            pages.detecting.style.animation = 'toShow 500ms ease-in-out forwards';
+            detect();
+        });
 
-        // 以下2行で一応device_scan.pyが実行できますが，うまく動作させることができません…
-        // const command = `${system.match(/Windows/) ? 'wsl' : ''} sudo python ./portScan/device_scan.py`;
-        // const result = execSync(command).toString();
 
-        // scanの戻り値をとりあえず出力
-        // console.log('port scan result:\n', result);
+        const detect = () => {
+            // {"hosts":["172.100.0.1","172.100.0.254","172.100.1.14","172.100.1.3","172.100.1.5","172.100.3.254"],"network":"172.100.0.0/22"}
+            // const json = {"hosts":["172.100.0.1","172.100.0.254","172.100.1.14","172.100.1.3","172.100.1.5","172.100.3.254"],"network":"172.100.0.0/22"};
+            const json = {"hosts":["172.100.0.1","172.100.0.254","172.100.1.14","172.100.1.3","172.100.1.5","172.100.3.254","test","hoge","foo","bar"],"network":"172.100.0.0/22"};
+
+            // 表示のテスト
+            json.hosts.map(address => {
+                // address
+                const p = document.createElement('p');
+                p.innerText = address;
+                pages.detecting.appendChild(p);
+            });
+        }
     };
 })();
