97f6de0bc1142e918a818e4ac469f399fa91d024
Haruto Hirakawa <31243896+JPNYKW@users.noreply.github.com>
2019-05-23 15:54:18 +0900

    fix/path

--- javascript/dom.js
+++ javascript/dom.js
@@ -34,7 +34,7 @@ let loginInfo = {};
             // {"hosts":["172.100.0.1","172.100.0.254","172.100.1.14","172.100.1.3","172.100.1.5","172.100.3.254"],"network":"172.100.0.0/22"}
             // const json = {"hosts":["172.100.0.1","172.100.0.254","172.100.1.14","172.100.1.3","172.100.1.5","172.100.3.254"],"network":"172.100.0.0/22"};
             setTimeout(() => {
-                const command = 'python3 ./portScan/device_scan.py';
+                const command = 'python3 ./python/device_scan.py';
                 const result = spawnSync(command, { shell: true });
                 const stdout = result.output[1].toString();
                 const stderr = result.output[2].toString();
